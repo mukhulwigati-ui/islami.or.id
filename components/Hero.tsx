de4c9583c122c32c9c7579f7db1cb9a73bd09ffc
naufalhawari001-stack <avirtualzonecom@gmail.com>
2026-07-28 09:36:22 +0700

    wakaf

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -59,7 +59,7 @@ export default function Hero({ initialBanners = [] }: HeroProps) {
     { name: 'Sedekah Subuh', icon: '/images/sedekah-subuh.jpg', href: '/campaign/sedekah-subuh', glowing: false },
     { name: 'Bencana', icon: '/images/bencana.webp', href: '/program?cat=bencana', glowing: false },
     { name: 'Fidyah', icon: '/images/fidyah.jpg', href: '/campaign/bayar-fidyah-untuk-dhuafa-pelosok', glowing: true },
-    { name: 'Wakaf', icon: '/images/wakaf.jpg', href: '/program?cat=wakaf', glowing: false },
+    { name: 'Wakaf', icon: '/images/wakaf.jpg', href: '/campaign/wakaf', glowing: false },
     { name: 'ORTA', icon: '/images/orta.png', href: '/campaign/jadi-orang-tua-asuh-selamatkan-masa-depan-ribuan-yatim', glowing: false },
     { name: 'Sedekah Jumat', icon: '/images/sedekah-jumat.png', href: '/program?cat=sedekah-jumat', glowing: false },
     { name: 'Kifarat', icon: '/images/kifarat.jpeg', href: '/program?cat=kifarat', glowing: false },
